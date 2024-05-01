83c49b8f6d1a67cf17998bfa67b95f4834a7653d
Hemapoojitha-ch <poojitha279@gmail.com>
2024-05-01 11:30:27 +0530

    Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import Home_Image from './../assets/Agro_icon/farmerandcrow.jpg'
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <img src={Home_Image} alt="Home" className='w-full h-90% mr-2'></img>
+      <div>
+        Resources
+        
+      </div>
+    </div>
   )
 }
